f4633a1c0212ff5eb2369bfdd08f84694d9b835a
jiminsub <jiminsub@gmail.com>
2016-08-01 08:45:29 +0900

    Initial commit

---
+++ src/main/java/com/minsub/java/java8/Java8Test.java
@@ -0,0 +1,10 @@
+package com.minsub.java.java8;
+
+/**
+ * Created by hmm1115222 on 2016-08-01.
+ */
+public class Java8Test {
+    public static void main(String[] args) {
+        System.out.println("Main Test");
+    }
+}
